678e5c2dff2e542c19364f02e93a87ff54bcf6b5
Jeizard <japhex@mail.ru>
2024-06-19 12:57:34 +0300

    feat: add backgroundColor field to OnBoardingItem

--- app/src/main/java/com/jeizard/onboarding/model/OnBoardingItem.kt
+++ app/src/main/java/com/jeizard/onboarding/model/OnBoardingItem.kt
@@ -2,7 +2,9 @@ package com.jeizard.onboarding.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
 import com.jeizard.onboarding.R
+import com.jeizard.onboarding.ui.theme.Yellow
 
 sealed class OnBoardingItem(
     @DrawableRes
@@ -10,11 +12,13 @@ sealed class OnBoardingItem(
     @StringRes
     val title: Int,
     @StringRes
-    val body: Int
+    val body: Int,
+    val backgroundColor: Color,
 ) {
     data object First : OnBoardingItem(
         image = R.drawable.onboarding_image_1,
         title = R.string.onboarding_title_1,
-        body = R.string.onboarding_body_1
+        body = R.string.onboarding_body_1,
+        backgroundColor = Yellow
     )
 }
