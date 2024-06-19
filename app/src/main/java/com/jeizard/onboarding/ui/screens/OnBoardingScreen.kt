d6ceb30f0dac63b4671d3805148694ac93f5da89
Jeizard <japhex@mail.ru>
2024-06-19 13:06:33 +0300

    feat: complete of OnBoardingScreen consisting of OnBoardingItem and BottomBar

--- app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
+++ app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
@@ -3,6 +3,7 @@ package com.jeizard.onboarding.ui.screens
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -17,15 +18,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,25 +39,71 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.jeizard.onboarding.model.OnBoardingItem
+import com.jeizard.onboarding.navigation.NavigationItem
+import com.jeizard.onboarding.navigation.Screen
+import com.jeizard.onboarding.ui.theme.OnBoardingTheme
 import com.jeizard.onboarding.ui.theme.Yellow
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-@Preview
-fun OnBoardingScreen() {
+fun OnBoardingScreen(navController: NavHostController) {
+    val scope = rememberCoroutineScope()
+
+    val items = listOf(
+        OnBoardingItem.First
+    )
+    val state = rememberPagerState(pageCount = { items.size })
+
+    OnBoardingTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(items[state.currentPage].backgroundColor)
+        ) {
+            HorizontalPager(
+                state = state,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(0.85f)
+            ) {
+                page -> OnBoardingItem(item = items[page])
+            }
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .weight(0.15f)){
+                BottomBar(
+                    size = items.size,
+                    selectedIndex = state.currentPage,
+                    backgroundColor = items[state.currentPage].backgroundColor,
+                    onNextClicked = {
+                        if(state.currentPage < (items.size - 1)){
+                            scope.launch {
+                                state.scrollToPage(state.currentPage + 1)
+                            }
+                        } else{
+                            navController.popBackStack()
+                            navController.navigate(NavigationItem.Home.route)
+                        }
+                    },
+                    onSkipClicked = {
+                        navController.popBackStack()
+                        navController.navigate(NavigationItem.Home.route)
+                    })
+            }
+        }
+    }
 }
 
 @Composable
-@Preview
-fun OnBoardingItem(
-) {
-    val item = OnBoardingItem.First
-
+fun OnBoardingItem(item: OnBoardingItem) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-            .background(Yellow)
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -64,11 +114,12 @@ fun OnBoardingItem(
                 text = stringResource(id = item.title),
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.primary,
-                fontSize = 24.sp)
+                fontSize = 24.sp,
+                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 12.dp))
             Text(
                 text = stringResource(id = item.body),
                 style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.primary,
+                color = MaterialTheme.colorScheme.primary.copy(0.9f),
                 fontSize = 18.sp)
         }
         Image(
@@ -79,18 +130,18 @@ fun OnBoardingItem(
 }
 
 @Composable
-@Preview
-fun BottomBar() {
-    val size = 4
-    val selectedIndex = 1
-    val backgroundColor = Yellow
-
+fun BottomBar(
+    size: Int,
+    selectedIndex: Int,
+    backgroundColor: Color,
+    onNextClicked: () -> Unit,
+    onSkipClicked: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(12.dp)
     ) {
-        Indicators(size, selectedIndex)
+        Indicators(size = size, selectedIndex = selectedIndex)
 
         Text(
             text = "Skip",
@@ -99,16 +150,24 @@ fun BottomBar() {
             modifier = Modifier
                 .align(Alignment.BottomStart)
                 .clickable {
-                    // onClick()
+                    onSkipClicked
                 }
         )
 
-        CircularProgressButton(size, selectedIndex, backgroundColor)
+        CircularProgressButton(
+            size = size,
+            selectedIndex = selectedIndex,
+            backgroundColor = backgroundColor,
+            onNextClicked = onNextClicked)
     }
 }
 
 @Composable
-fun BoxScope.CircularProgressButton(size: Int, selectedIndex: Int, backgroundColor: Color) {
+fun BoxScope.CircularProgressButton(
+    size: Int,
+    selectedIndex: Int,
+    backgroundColor: Color,
+    onNextClicked: () -> Unit) {
     Box(
         modifier = Modifier
             .size(58.dp)
@@ -125,7 +184,7 @@ fun BoxScope.CircularProgressButton(size: Int, selectedIndex: Int, backgroundCol
         )
 
         FloatingActionButton(
-            onClick = { /*TODO*/ },
+            onClick = { onNextClicked },
             modifier = Modifier
                 .size(42.dp)
                 .align(Alignment.Center),
@@ -134,7 +193,7 @@ fun BoxScope.CircularProgressButton(size: Int, selectedIndex: Int, backgroundCol
             shape = CircleShape
         ) {
             Icon(
-                imageVector = Icons.Outlined.KeyboardArrowRight,
+                imageVector = Icons.Rounded.KeyboardArrowRight,
                 contentDescription = "ArrowRightIcon"
             )
         }
@@ -171,4 +230,14 @@ fun Indicator(isSelected: Boolean) {
                 else MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)
             )
     )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun FirstOnBoardingScreenPreview() {
+    OnBoardingTheme {
+        Box(modifier = Modifier.background(Yellow)) {
+            OnBoardingItem(item = OnBoardingItem.First)
+        }
+    }
 }
