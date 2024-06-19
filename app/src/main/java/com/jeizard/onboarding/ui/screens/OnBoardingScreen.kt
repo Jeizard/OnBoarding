f716b11bc6f15dcdf7ef5765e958694c4a07db30
Jeizard <japhex@mail.ru>
2024-06-19 11:04:31 +0300

    feat: create indicator of page

---
+++ app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
@@ -0,0 +1,43 @@
+package com.jeizard.onboarding.ui.screens
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Composable
+@Preview
+fun OnBoardingScreen() {
+}
+
+@Composable
+@Preview
+fun Indicator() {
+    val isSelected = true
+
+    val width = animateDpAsState(
+        targetValue = if (isSelected) 24.dp else 8.dp,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy),
+        label = "IndicatorWidthAnimation"
+    )
+
+    Box(
+        modifier = Modifier
+            .height(8.dp)
+            .width(width.value)
+            .clip(CircleShape)
+            .background(
+                if (isSelected) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)
+            )
+    )
+}
