5458c42424a3ddacf72f23d4807867349e3b7974
Jeizard <japhex@mail.ru>
2024-06-19 11:26:14 +0300

    feat: create a bottom bar combining page indicators and next button

--- app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
+++ app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -18,10 +21,12 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jeizard.onboarding.ui.theme.Yellow
@@ -33,14 +38,39 @@ fun OnBoardingScreen() {
 
 @Composable
 @Preview
-fun CircularProgressButton() {
+fun BottomBar() {
     val size = 4
     val selectedIndex = 1
     val backgroundColor = Yellow
 
     Box(
         modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    ) {
+        Indicators(size, selectedIndex)
+
+        Text(
+            text = "Skip",
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.primary.copy(0.7f),
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .clickable {
+                    // onClick()
+                }
+        )
+
+        CircularProgressButton(size, selectedIndex, backgroundColor)
+    }
+}
+
+@Composable
+fun BoxScope.CircularProgressButton(size: Int, selectedIndex: Int, backgroundColor: Color) {
+    Box(
+        modifier = Modifier
             .size(58.dp)
+            .align(Alignment.CenterEnd)
     ) {
         CircularProgressIndicator(
             progress = ((selectedIndex + 1).toFloat() / size.toFloat()),
@@ -70,21 +100,17 @@ fun CircularProgressButton() {
 }
 
 @Composable
-@Preview
-fun Indicators() {
-    val size = 4
-    val selectedIndex = 1
-
+fun BoxScope.Indicators(size: Int, selectedIndex: Int) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.align(Alignment.TopStart)
     ) {
         repeat(size){
             Indicator(isSelected = it == selectedIndex)
         }
     }
 }
-
 @Composable
 fun Indicator(isSelected: Boolean) {
     val width = animateDpAsState(
