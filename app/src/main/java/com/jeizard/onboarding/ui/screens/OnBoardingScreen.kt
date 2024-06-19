c9db887ebff382ecb9739d87a089f0edc1798df5
Jeizard <japhex@mail.ru>
2024-06-19 11:42:44 +0300

    feat: create display of the OnBoarding item

--- app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
+++ app/src/main/java/com/jeizard/onboarding/ui/screens/OnBoardingScreen.kt
@@ -3,12 +3,15 @@ package com.jeizard.onboarding.ui.screens
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -27,13 +30,52 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jeizard.onboarding.model.OnBoardingItem
 import com.jeizard.onboarding.ui.theme.Yellow
 
 @Composable
 @Preview
 fun OnBoardingScreen() {
+}
+
+@Composable
+@Preview
+fun OnBoardingItem(
+) {
+    val item = OnBoardingItem.First
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+            .background(Yellow)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(25.dp)
+        ) {
+            Text(
+                text = stringResource(id = item.title),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 24.sp)
+            Text(
+                text = stringResource(id = item.body),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 18.sp)
+        }
+        Image(
+            painter = painterResource(id = item.image),
+            contentDescription = "OnBoardingImage",
+            modifier = Modifier.fillMaxWidth())
+    }
 }
 
 @Composable
